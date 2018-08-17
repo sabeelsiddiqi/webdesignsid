cd9a37d06cdae3e947461aa02b522997c4ad5036
sabeelsiddiqi@gmail.com <sabeelsiddiqi@gmail.com>
2018-08-16 22:47:33 -0400

    uploaded site

--- js/freelancer.js
+++ js/freelancer.js
@@ -44,22 +44,26 @@
       $("#mainNav").removeClass("navbar-shrink");
     }
   };
+
+
+
+
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
   // Modal popup$(function () {
-  $('.portfolio-item').magnificPopup({
-    type: 'inline',
-    preloader: false,
-    focus: '#username',
-    modal: true
-  });
-  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
-    e.preventDefault();
-    $.magnificPopup.close();
-  });
+  // $('.portfolio-item').magnificPopup({
+  //   type: 'inline',
+  //   preloader: false,
+  //   focus: '#username',
+  //   modal: true
+  // });
+  // $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+  //   e.preventDefault();
+  //   $.magnificPopup.close();
+  // });
 
   // Floating label headings for the contact form
   $(function() {
---
+++ js/functions.js
@@ -0,0 +1,10 @@
+var portfolioitem1 = function(){
+  console.log("CLICK")
+
+};
+
+function openInNewTab(url) {
+  console.log('TEST')
+var win = window.open(url, '_blank');
+win.focus();
+}
